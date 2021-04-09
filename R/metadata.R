f673b2bebc03a5952fbb24c980a0d356c47ee3f9
rqthomas <rqthomas@vt.edu>
2021-04-09 00:49:22 +0000

    adding netcdf support

--- R/metadata.R
+++ R/metadata.R
@@ -1,7 +1,6 @@
 #' Write metadata from template
 #' 
 #' @param forecast_file full path of forecast file
-#' 
 #' @export
 #' @examples 
 write_meta_template <- function(forecast_file){
@@ -30,24 +29,80 @@ generate_metadata <- function(forecast_file,
   
   dir <- dirname(forecast_file)
   
-  forecast_file_name_base <- paste0(tools::file_path_sans_ext(tools::file_path_sans_ext(basename(forecast_file))),".yml")
+  forecast_file_name_base <- tools::file_path_sans_ext(tools::file_path_sans_ext(basename(forecast_file)))
   metadata <- yaml::read_yaml(metadata_yaml)
   
   if(tools::file_ext(forecast_file) %in% c("csv", "gz")){
     forecast <- readr::read_csv(forecast_file)
-  } else {
-    #NEED TO ADD NETCDF SUPPORT
-    forecast <- NULL
+  } else if(tools::file_ext(forecast_file) %in% c("nc")){ #if file is nc
+      
+      nc <- ncdf4::nc_open(forecast_file)
+      siteID <- ncdf4::ncvar_get(nc, "siteID")
+      time <- as.integer(ncdf4::ncvar_get(nc, "time"))
+      #tustr<-strsplit(ncdf4::ncatt_get(nc, varid = "time", "units")$value, " ")
+      #time <-lubridate::as_date(time,origin=unlist(tustr)[3])
+      t_string <- strsplit(ncdf4::ncatt_get(nc, varid = "time", "units")$value, " ")[[1]]
+      if(t_string[1] == "days"){
+        tustr<-strsplit(ncdf4::ncatt_get(nc, varid = "time", "units")$value, " ")
+        time <-lubridate::as_date(time,origin=unlist(tustr)[3])
+      }else{
+        tustr <- lubridate::as_datetime(strsplit(ncdf4::ncatt_get(nc, varid = "time", "units")$value, " ")[[1]][3])
+        time <- as.POSIXct.numeric(time, origin = tustr)
+      } 
+      
+      target_variables = c("oxygen", 
+                           "temperature", 
+                           "richness",
+                           "abundance", 
+                           "nee",
+                           "le", 
+                           "vswc",
+                           "gcc_90",
+                           "ixodes_scapularis",
+                           "amblyomma_americanum")
+      
+      reps_col <- "ensemble"
+      
+      targets <- names(nc$var)[which(names(nc$var) %in% target_variables)]
+      combined_forecast <- NULL
+      for(j in 1:length(targets)){
+        forecast_targets <- ncdf4::ncvar_get(nc, targets[j])
+        for(i in 1:length(siteID)){
+          tmp <- forecast_targets[ ,i ,]
+          d <- cbind(time, as.data.frame(tmp))
+          names(d) <- c("time", seq(1,dim(tmp)[2]))
+          d <- d %>%
+            tidyr::pivot_longer(-time, names_to = reps_col, values_to = "value") %>%
+            dplyr::mutate(siteID = siteID[i],
+                          variable = targets[j])
+          combined_forecast <- dplyr::bind_rows(combined_forecast, d)
+        }
+      }
+      ncdf4::nc_close(nc)
+      combined_forecast <- combined_forecast %>%
+        tidyr::pivot_wider(names_from = variable, values_from = value) %>% 
+        dplyr::mutate(filename = basename(file_in))
+      
+      forecast <- combined_forecast
   }
   
   theme <- unlist(stringr::str_split(stringr::str_split(forecast_file, "-")[[1]][1], "_")[[1]][1])
   team_name <- unlist(stringr::str_split(forecast_file_name_base, "-"))[5]
   
-  attributes <- readr::read_csv(system.file(paste0("extdata/",theme, "metadata_attributes.csv"), package="neon4cast"))
+  attributes <- readr::read_csv(system.file(paste0("extdata/",theme, "_metadata_attributes.csv"), package="neon4cast"))
   if("ensemble" %in% names(forecast)){
     attributes <- dplyr::filter(attributes, attributeName != "statistic")
+    num_variables <- length(which(stringr::str_detect(attributes$attributeDefinition,"variable")))
+    #' use `EML` package to build the attribute list
+    attrList <- EML::set_attributes(attributes, 
+                                    col_classes = c("Date", "numeric", "character","numeric","numeric", 
+                                                    rep("numeric", num_variables)))
   }else if("statistic" %in% names(forecast)){
     attributes <- dplyr::filter(attributes, attributeName != "ensemble")
+    #' use `EML` package to build the attribute list
+    attrList <- EML::set_attributes(attributes, 
+                                    col_classes = c("Date", "character", "character","numeric","numeric", 
+                                                    rep("numeric", num_variables)))
   }else{
     message("Column names in file does not have ensemble or statistic column")
   }
@@ -61,16 +116,14 @@ generate_metadata <- function(forecast_file,
   )
 
   #' use `EML` package to build the attribute list
-  attrList <- EML::set_attributes(attributes, 
-                                  col_classes = c("Date", "numeric", "character","numeric","numeric", 
-                                                  "numeric","numeric", "numeric","numeric"))
+
   
   
   #' use `EML` package to build the physical list
   physical <- EML::set_physical(forecast_file)
   
   #' use `EML` package to dataTable
-  dataTable <- eml$dataTable(
+  dataTable <- EML::eml$dataTable(
     entityName = "forecast",  ## this is a standard name to allow us to distinguish this entity from 
     entityDescription = entityDescription_text,
     physical = physical,
@@ -91,21 +144,21 @@ generate_metadata <- function(forecast_file,
                    temporalCoverage = temporalCoverage)
   
   #'Create the dataset EML
-  dataset <- eml$dataset(
+  dataset <- EML::eml$dataset(
     title = "Daily persistence null forecast for nee and lee",
     creator = metadata$team_list,
     contact = metadata$team_list[[1]],
-    pubDate = as_date(forecast_issue_time),
+    pubDate = lubridate::as_date(forecast_issue_time),
     intellectualRights = "https://creativecommons.org/licenses/by/4.0/",
     dataTable = dataTable,
     coverage = coverage
   )
   
-  metadata$metadata$forecast$forecast_issue_time <- as_date(forecast_issue_time)
+  metadata$metadata$forecast$forecast_issue_time <- lubridate::as_date(forecast_issue_time)
   metadata$metadata$forecast$forecast_iteration_id <- forecast_iteration_id
   metadata$metadata$forecast$forecast_project_id <- team_name
   
-  my_eml <- eml$eml(dataset = dataset,
+  my_eml <- EML::eml$eml(dataset = dataset,
                     additionalMetadata = eml$additionalMetadata(metadata = metadata$metadata),
                     packageId = forecast_iteration_id , 
                     system = "datetime"  ## system used to generate packageId
@@ -113,6 +166,7 @@ generate_metadata <- function(forecast_file,
   
   #'Check base EML
   if(!EML::eml_validate(my_eml)){
+    EML::eml_validate(my_eml)
     message("Error in EML metadata")
   }
   
@@ -142,7 +196,6 @@ neon_geographic_coverage <- function(sites){
   names(geo) <- site_ids
   geo[sites]
 }
-
 
 theme_sites <- function(theme){
   switch(theme,
