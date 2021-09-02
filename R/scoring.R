d4012cf78abd29b33d344bafcb8a9454b1ba85d1
rqthomas <rqthomas@vt.edu>
2021-09-02 15:25:30 -0400

    adding logs score to table

--- R/scoring.R
+++ R/scoring.R
@@ -31,13 +31,13 @@ score <- function(forecast,
   download_url <- paste0("https://data.ecoforecast.org/targets/",
                          theme, "/", target_file)
   target <- readr::read_csv(download_url)
-  crps_score(forecast, target)
+  crps_logs_score(forecast, target)
   
 }
 
 # guess_theme 
 
-crps_score <- function(forecast, 
+crps_logs_score <- function(forecast, 
                        target,
                        grouping_variables = c("siteID", "time"),
                        target_variables = c("oxygen", 
@@ -69,13 +69,24 @@ crps_score <- function(forecast,
   target <- target %>% dplyr::select(tidyselect::any_of(variables))
   
   ## Teach crps to treat any NA observations as NA scores:
-  scoring_fn_ensemble <- function(y, dat) {
+  scoring_crps_ensemble <- function(y, dat) {
     tryCatch(scoringRules::crps_sample(y, dat),
              error = function(e) NA_real_, finally = NA_real_)
   }
   
-  scoring_fn_stat <- function(y, mean, sd) {
+  scoring_crps_stat <- function(y, mean, sd) {
     tryCatch(scoringRules::crps_norm(y, mean = mean, sd = sd),
+             error = function(e) NA_real_, finally = NA_real_)
+  }
+  
+  ## Teach crps to treat any NA observations as NA scores:
+  scoring_logs_ensemble <- function(y, dat) {
+    tryCatch(scoringRules::logs_sample(y, dat),
+             error = function(e) NA_real_, finally = NA_real_)
+  }
+  
+  scoring_logs_stat <- function(y, mean, sd) {
+    tryCatch(scoringRules::logs_norm(y, mean = mean, sd = sd),
              error = function(e) NA_real_, finally = NA_real_)
   }
   
@@ -93,7 +104,9 @@ crps_score <- function(forecast,
     
     dplyr::inner_join(forecast_long, target_long, by = c(grouping_variables, "target"))  %>% 
       dplyr::group_by(dplyr::across(tidyselect::any_of(c(grouping_variables, "target", "horizon", "team", "forest_start_time", "theme")))) %>% 
-      dplyr::summarise(score = scoring_fn_ensemble(observed[[1]], predicted),
+      dplyr::summarise(crps = scoring_crps_ensemble(observed[[1]], predicted),
+                       logs = scoring_logs_ensemble(observed[[1]], predicted),
+                       
                 .groups = "drop")
     
   } else {
@@ -103,7 +116,8 @@ crps_score <- function(forecast,
       dplyr::inner_join(target_long, by = c(grouping_variables, "target"))  %>% 
       dplyr::group_by(dplyr::across(dplyr::any_of(
         c(grouping_variables, "target", "horizon", "team", "forest_start_time", "theme")))) %>% 
-      dplyr::summarise(score = scoring_fn_stat(observed[[1]], mean, sd),
+      dplyr::summarise(crps = scoring_crps_stat(observed[[1]], mean, sd),
+                       logs = scoring_logs_stat(observed[[1]], mean, sd),
                 .groups = "drop")
     
   }
@@ -134,7 +148,7 @@ score_it <- function(targets_file,
   forecasts <- lapply(forecast_files, read_forecast)
   
   scores <- lapply(forecasts, 
-                   crps_score, 
+                   crps_logs_score, 
                    target = target,  
                    target_variables = target_variables, 
                    grouping_variables = c(grouping_variables),
