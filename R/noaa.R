b80c504003bccb54f920963ad37e66bb4afa40e9
rqthomas <rqthomas@vt.edu>
2022-03-01 03:21:20 +0000

    fixing documeation

--- R/noaa.R
+++ R/noaa.R
@@ -1,3 +1,62 @@
+#' Download NOAA Weather forecasts for NEON sites from the EFI server
+#'  
+#' @param siteID vector of 4-character NEON siteIDs
+#' @param interval Time interval for the forecast
+#' @param date start date for the forecast
+#' @param cycle NOAA hour-cycle (first hour of the forecast),
+#'  options are "00", "06", "12", "18"; only the "00" forecasts
+#'  run 35 days into future.
+#' @param dir storage location.  Use tempdir unless you want to keep this 
+#' data around on your computer, in which case, `neonstore::neon_dir()` might
+#' be a convenient choice.
+#' @export
+#' @examples 
+#' download_noaa("ABBY")
+download_noaa <- function(siteID, 
+                          interval = "6hr",
+                          date = Sys.Date()-2, 
+                          cycle = "00", 
+                          dir = tempdir()){
+  lapply(siteID, download_noaa_, interval, date, cycle, dir)
+  invisible(dir)
+}
+download_noaa_ <- function(siteID, 
+                           interval = "6hr",
+                           date = Sys.Date()-2, 
+                           cycle = "00", 
+                           dir = tempdir()){
+  
+  noaadir <- file.path(dir, "noaa")
+  dir.create(noaadir, FALSE, TRUE)
+  prefix <- paste("noaa", paste0("NOAAGEFS_", interval), 
+                  siteID, date, cycle, sep="/")
+  
+  #GENERALIZATION:  Specific AWS info
+  object <- aws.s3::get_bucket("drivers",
+                               prefix = prefix,
+                               region = "data",
+                               base_url = "ecoforecast.org")
+  
+  #data <- purrr::map_chr(object, ~ .x$Key)
+  
+  for(i in seq_along(object)){
+    #GENERALIZATION:  Specific AWS info
+    aws.s3::save_object(object[[i]], 
+                        bucket = "drivers", 
+                        file = file.path(noaadir, object[[i]]$Key),
+                        region = "data",
+                        base_url = "ecoforecast.org")
+  }
+}
+
+#' Stack downloaded NOAA files
+#' 
+#' @inheritParams download_noaa
+#' @param model NOAA model: NOAAGEFS_1hr, NOAAGEFS_1hr_stacked, NOAAGEFS_1hr_stacked_average
+#' @param forecast_date Include only forecasts issued on this date
+#' @examples 
+#' stack_noaa()
+#' @export
 stack_noaa <- function(dir = tempdir(), model, forecast_date = NULL) {
   
   if(!stringr::str_detect(model,"averaged")){
@@ -66,3 +125,40 @@ stack_noaa <- function(dir = tempdir(), model, forecast_date = NULL) {
   
   return(out)
 }
+
+#' Download stacked NOAA data from s3 bucket
+#'
+#' @param dir full path to working directory
+#' @param config flare configuration object
+#' @param averaged logistical; TRUE = download averaged stacked forecast
+#'
+#' @return
+#' @export
+#'
+get_stacked_noaa_s3 <- function(dir, site, averaged = TRUE, s3_region = Sys.getenv("AWS_DEFAULT_REGION")){
+  
+  if(averaged){
+    download_s3_objects(dir, bucket = "drivers", prefix = file.path("noaa/NOAAGEFS_1hr_stacked_average",site), s3_region)
+  }else{
+    download_s3_objects(dir, bucket = "drivers", prefix = file.path("noaa/NOAAGEFS_1hr_stacked",site), s3_region)
+  }
+}
+
+#' Download driver forecasts from s3 bucket
+#'
+#' @param dir full path to working directory
+#' @param forecast_path relative path of the driver forecast (relative to driver directory or bucket)
+#'
+#' @return
+#' @export
+#'
+get_noaa_forecast_s3 <- function(dir, model, site, date, cycle, s3_region = Sys.getenv("AWS_DEFAULT_REGION")){
+  
+  download_s3_objects(dir,
+                      bucket = "drivers",
+                      prefix = file.path("noaa",model, site, date, cycle),
+                      s3_region = s3_region)
+}
+
+
+
