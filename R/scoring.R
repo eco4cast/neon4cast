80f8fd75713fdbf4a34c62bc0cdd574b5fc01c50
Carl <cboettig@gmail.com>
2022-03-08 22:01:24 +0000

    both interval and horizon are integers

--- R/scoring.R
+++ R/scoring.R
@@ -301,7 +301,8 @@ include_horizon <- function(df, allow_difftime = getOption("neon4cast.allow_diff
     mutate(horizon = time - forecast_start_time)
   
   if(!allow_difftime){
-    df %>% mutate(horizon = as.integer(horizon))
+    df <- df %>% mutate(horizon = as.integer(horizon),
+                        interval = as.integer(interval))
   }
   df
 }
