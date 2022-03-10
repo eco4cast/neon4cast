6cd31897f8b8b2d177aa068c78df7588c86387f7
Carl <cboettig@gmail.com>
2022-03-10 20:13:49 +0000

    streamline mean scores

--- R/mean_scores.R
+++ R/mean_scores.R
@@ -1,140 +1,35 @@
 
-db_import <- function(data, dir = tempfile(), use_db = getOption("neon4cast_usedb", FALSE)){
-  
-  df <- data %>% 
-    dplyr::select(theme, site, target, time, team, forecast_start_time, crps, logs)
-  
-  ## Hmm, SQLite doesn't have date format, might be better as character than as double...
-  df <- df %>% dplyr::mutate(time = as.character(time), 
-                forecast_start_time = as.character(forecast_start_time))
-  
-  
-  ## Cannot use duckdb until tidyr::fill issue resolved in dbplyr
-  if(use_db) {
-    con <- sql_db(dir)
-    DBI::dbWriteTable(con, "combined", df, overwrite=TRUE)
-    df <- dplyr::tbl(con, "combined")
-  }
-  
-  df
-}
 
-
-sql_db <- function(dir){
-  con <- getOption("neon4cast_con")
-  if(is.null(con)) {
-    con <- DBI::dbConnect(RSQLite::SQLite(), dir)
-  }
-  ## cache connection so not garbage-collected
-  options(neon4cast_con = con) 
-  
-  con
-}
-
-#' fill_scores
-#' 
-#' To compare teams which issue forecasts of multiple observations, we need 
-#' a rule for dealing with cases where one team makes predictions for 
-#' certain observations at a given forecast horizon when another team does not.
-#' For instance, teams may use different horizon lengths (forecast different
-#' number of days into the future).  Alternately, two teams may both predict
-#' the same observation (target, time, site) but the one team does so 
-#' much farther in advance than the other.  These miss-matches create
-#' "implicit" missing observations relative to other scores.  If we simply omit
-#' missing values, teams have little incentive to make a full set of predictions 
-#' -- dropping missing values while making one good prediction could lead to a much
-#' much better score.  
-#' 
-#' To address this, we use a null score as a reference.  Teams are scored for all observations
-#' and forecast horizons for which the null forecast contains a prediction.  
-#' If the team has made a forecast of a given observation but only on a longer horizon, 
-#' we will re-use that prediction when comparing against the prediction on the shorter horizon as well.
-#' If no prior forecast is available, we will fill the missing prediction with that of the null
-#' forecast.
-#' @param null_team name of the null team
-#' @param df data.frame of raw scores.  Must have columns:
-#' `theme`, `site`, `target`, `time`, `team`, `forecast_start_time`,
-#'  `crps` & `logs`.
 #' @export
-fill_scores <- function(df, null_team = "EFInull"){
-  raw_scores <- db_import(df)
-  
-  ## Turn implicit NAs to explicit ones:
-  na_filled <- na_fill(raw_scores)
-  ## Fill missing values with older forecasts of same observations made by same team
-  self_filled <- self_fill(na_filled)
-  
-  ## Fill remaining missing values with those from the null_team:
-  null_filled <- null_fill(self_filled, null_team = null_team)
+fill_scores <- function(df, null_team = "EFInull") {
+  null <- df %>% 
+    filter(team == null_team) %>%
+    select("theme", "target", "x","y","z", "site", "time",
+           "forecast_start_time", "crps", "logs")
+  all <- tidyr::expand_grid(null, distinct(df,team))
+  na_filled <- left_join(all, df,
+                         by = c("theme", "team", "target", "x","y","z",
+                                "site", "time", "forecast_start_time"),
+                         suffix = c("_null", "_team"))
+  null_filled <- na_filled %>% mutate(
+    crps = case_when(is.na(crps_team) ~ crps_null,
+                     !is.na(crps_team) ~ crps_team),
+    logs = case_when(is.na(logs_team) ~ logs_null,
+                     !is.na(logs_team) ~ logs_team))%>% 
+    select(-crps_null, -logs_null)
   
   null_filled
-}  
-  
-  
-na_fill <- function(df, null = "EFInull"){
-  
-  ## expand a table to all possible observations (target, site, time)
-  ## for each team, for each forecast_start_time:
-  predicted_by_null <- df %>% 
-    filter(team == null) %>% 
-    select(theme, target, site, time, forecast_start_time) %>%
-    distinct() %>% collect()
-  team <- df %>% select(team) %>% distinct() %>% pull(team)
-  
-  all <- tidyr::expand_grid(predicted_by_null, team)
-  
-  ## Use this list to make explicit NA for any observation for which a forecast was not provided
-  na_filled <- df %>% right_join(all, copy=TRUE)
-  
-  na_filled
 }
 
-self_fill <- function(na_filled){
-  
-  if(inherits(na_filled, "tbl_sql")){
-    arrange <- dbplyr::window_order
-  }
-  
-  ## Fill in any missing observation with the most recent forecast made prior to the start_time
-  self_filled <- na_filled %>%
-   # dbplyr::window_order(theme, team, target, site, time, forecast_start_time) %>%
-    arrange(theme, team, target, site, time, forecast_start_time) %>%
-    group_by(theme, team, target, site, time) %>% 
-    fill(crps, logs, .direction="up") %>%       ## not duckdb-compatible!
-    ungroup() %>%
-    rename(crps_self = crps, logs_self = logs)
-  
-  ## Join onto original table so we have the original column of NAs + filled column
-  na_filled %>% left_join(self_filled)
 
-  
-}
 
-null_fill <- function(self_filled, null_team = "EFInull"){
-  ## We will now fill all remaining NAs using the NULL forecast.
-  null_score <- self_filled %>% ungroup() %>%
-    filter(team == null_team) %>% 
-    rename(crps_null = crps, logs_null = logs) %>%  
-    select("theme", "target", "site", "forecast_start_time",
-           "time", "crps_null", "logs_null")
-  
-  null_filled <- self_filled %>%
-    left_join(null_score) %>% # add null-score as a separate column
-    ## Add columns which fill any remaining NAs after self-fill was applied:
-    mutate(filled_crps = case_when(is.na(crps_self) ~ crps_null,
-                                   !is.na(crps_self) ~ crps_self),
-           filled_logs = case_when(is.na(logs_self) ~ logs_null,
-                                   !is.na(logs_self) ~ logs_self),
-  ## Add columns which fill NAs of original expanded data (no self-fill) with null,         
-           null_filled_crps = case_when(is.na(crps) ~ crps_null,
-                                        !is.na(crps) ~ crps),
-           null_filled_logs = case_when(is.na(logs) ~ logs_null,
-                                        !is.na(logs) ~ logs)
-           )  %>% select(-crps_null, -logs_null)
-  
-  ## Restore date-time formatting (needs duckdb)
-  null_filled
-  }
+
+
+
+
+
+
+
 
 #' mean_scores
 #' 
@@ -152,19 +47,14 @@ null_fill <- function(self_filled, null_team = "EFInull"){
 #' @export
 mean_scores <- function(df){
 
-scores <- df %>% 
-  group_by(team, target) %>% 
-  summarise(mean_crps = mean(filled_crps, na.rm =TRUE), 
-            mean_log = mean(filled_logs, na.rm =TRUE),
-            mean_null_filled_crps = mean(null_filled_crps, na.rm =TRUE),
-            mean_null_filled_logs = mean(null_filled_logs, na.rm =TRUE),
-            na_raw = sum(is.na(crps)),
-            na_self  = sum(is.na(crps_self)),
-            na_filled = sum(is.na(filled_crps)),
-            .groups = "drop") %>%
-  collect() %>% arrange(mean_crps)
-
-scores
+  df %>% 
+    group_by(team, target) %>%
+    summarise(crps = mean(crps),
+              logs = mean(logs),
+              sample_crps = mean(crps_team, na.rm=TRUE),
+              sample_logs = mean(logs_team, na.rm=TRUE),
+              percent_NA = mean(is.na(crps_team)), .groups = "drop") 
+  
 }
 
 
