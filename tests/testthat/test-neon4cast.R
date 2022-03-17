32c7ca9968407044a576690bfa5c7391c6544afa
Carl <cboettig@gmail.com>
2022-03-17 23:31:59 +0000

    import and rexport scoring and reading

--- tests/testthat/test-neon4cast.R
+++ tests/testthat/test-neon4cast.R
@@ -14,7 +14,7 @@ expect_true(is(df, "tbl"))
 
 
 nc_fc <- read_forecast("https://data.ecoforecast.org/forecasts/terrestrial_30min/terrestrial_30min-2022-01-01-hist30min.nc")
-score(nc_fc, , "https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz", theme="terrestrial_30min")
+score(nc_fc, "https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz", theme="terrestrial_30min")
 ## ------------------------------------------------------------------------------------------------------------
 #download_noaa("ABBY")
 #abby <- stack_noaa()
