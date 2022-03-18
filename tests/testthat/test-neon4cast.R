0a629a00aa07e827f85ed22aea520b57a197d403
Carl <cboettig@gmail.com>
2022-03-18 04:15:09 +0000

    very minimial testing here

--- tests/testthat/test-neon4cast.R
+++ tests/testthat/test-neon4cast.R
@@ -8,21 +8,13 @@ forecast_file <- system.file("extdata/aquatics-2021-02-01-EFInull.csv.gz",
 status <- forecast_output_validator(forecast_file)
 expect_true(status)
 
-## ------------------------------------------------------------------------------------------------------------
 df <- score(forecast_file, "https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz", theme="aquatics")
 expect_true(is(df, "tbl"))
 
 
-nc_fc <- read_forecast("https://data.ecoforecast.org/forecasts/terrestrial_30min/terrestrial_30min-2022-01-01-hist30min.nc")
-score(nc_fc, "https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz", theme="terrestrial_30min")
-## ------------------------------------------------------------------------------------------------------------
-#download_noaa("ABBY")
-#abby <- stack_noaa()
-#expect_s3_class(abby, "data.frame")
-
-
-## ------------------------------------------------------------------------------------------------------------
-#status <- submit(forecast_file)
-#expect_null(status)
+fc <- "https://data.ecoforecast.org/forecasts/aquatics/aquatics-2020-09-01-EFInull.csv.gz"
+df <- score(fc, 
+            "https://data.ecoforecast.org/targets/aquatics/aquatics-targets.csv.gz")
+expect_true(inherits(df, "data.frame"))
 
 })
