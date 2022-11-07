2b15243852caa93f5e706dea73d29d39abbd64c5
QuinnThomas <rqthomas@vt.edu>
2022-11-07 11:17:23 -0500

    Update combine_scores.R

--- R/combine_scores.R
+++ R/combine_scores.R
@@ -1,6 +1,6 @@
 #' Combining scores from a theme together
 #'
-#' @param x theme name (optional)
+#' @param theme theme name
 #' @param collect TRUE/FALSE to download results
 #' @return a data.frame of scores
 #' @export
@@ -18,6 +18,49 @@ combined_scores <- function(theme, collect = TRUE){
   }
   on.exit(neon4cast:::unset_arrow_vars(vars))
   ds
+}
+
+#' Calculating forecast challenge submission statistics
+#'
+#' @param themes theme names 
+#' @return a data.frame of challenge statistics
+#' @export
+
+theme_statistics <- function(themes){
+  
+theme_stats <- purrr::map_dfr(themes, function(theme){
+  
+  message(theme)
+  
+  theme_scores <- neon4cast::combined_scores(theme = theme, collect = FALSE)
+  
+  teams <- theme_scores |> 
+    dplyr::summarise(n = dplyr::n_distinct(model_id)) |> 
+    dplyr::collect() |> 
+    dplyr::pull(n)
+  
+  forecasts <- theme_scores |> 
+    dplyr::select(model_id,reference_datetime, variable) |> 
+    dplyr::distinct() |> 
+    dplyr::summarise(n = dplyr::n(), .groups = "drop") |> 
+    dplyr::summarise(total = sum(n)) |> 
+    dplyr::collect() |> 
+    dplyr::pull(total)
+  
+  forecast_obs <- theme_scores |> 
+    dplyr::filter(!is.na(crps)) |> 
+    dplyr::summarise(n = n(), .groups = "drop") |> 
+    dplyr::summarise(total = sum(n)) |> 
+    dplyr::collect() |> 
+    dplyr::pull(total)
+  
+  output <- tibble::tibble(theme = theme, n_teams = teams, n_submissions = forecasts, n_obs_forecasts_pairs = forecast_obs)
+  
+  return(output)
+  })
+
+return(theme_stats)
+
 }
 globalVariables("theme", "neon4cast")
 
