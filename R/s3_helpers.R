9914adda276a1e6abf691fa643ec66def72f4890
rqthomas <rqthomas@vt.edu>
2023-11-03 16:28:49 -0400

    updating

--- R/s3_helpers.R
+++ R/s3_helpers.R
@@ -1,42 +1,20 @@
-# DEPRECATE and mark for removal
+get_target <- function(variable, duration, project_id = "neon4cast", lazy = FALSE){
 
-#' Download target data from s3
-#'
-#' @param dir full path to working directory
-#' @param theme forecast theme
-#' @param s3_region s3 region
-#' @export
-#'
-get_target <- function(dir, theme, s3_region = Sys.getenv("AWS_DEFAULT_REGION")){
-  download_s3_objects(dir,
-                      bucket = "neon4cast-targets",
-                      prefix = theme)
-}
+  s3_targets <- arrow::s3_bucket("bio230014-bucket01/challenges/targets", endpoint_override = "sdsc.osn.xsede.org")
 
-####
+  target <- arrow::open_csv_dataset(s3_targets,
+                                    schema = arrow::schema(
+                                      project_id = arrow::string(),
+                                      site_id = arrow::string(),
+                                      datetime = arrow::timestamp(unit = "ns", timezone = "UTC"),
+                                      duration = arrow::string(),
+                                      variable = arrow::string(),
+                                      observation = arrow::float()),
+                                    skip = 1) |>
+    dplyr::filter(variable %in% variable,
+                  duration == duration,
+                  project_id == project_id)
 
-download_s3_objects <- function(dir, bucket, prefix, s3_region = Sys.getenv("AWS_DEFAULT_REGION")){
-  
-  files <- aws.s3::get_bucket(bucket = bucket,
-                              prefix = prefix,
-                              region = s3_region,
-                              use_https = as.logical(Sys.getenv("USE_HTTPS")),
-                              max = Inf)
-  keys <- vapply(files, `[[`, "", "Key", USE.NAMES = FALSE)
-  empty <- grepl("/$", keys)
-  keys <- keys[!empty]
-  files_present <- TRUE
-  if(length(keys) > 0){
-    for(i in 1:length(keys)){
-      aws.s3::save_object(object = keys[i],
-                          bucket = bucket,
-                          file = file.path(dir, bucket, keys[i]),
-                          region = s3_region,
-                          use_https = as.logical(Sys.getenv("USE_HTTPS")))
-    }
-  }else{
-    message("Requested files are not available on the s3 bucket")
-    files_present <- FALSE
-  }
-  invisible(files_present)
+  if(!lazy) target <- target |> dplyr::collect()
+
 }
--- R/submit.R
+++ R/submit.R
@@ -1,18 +1,18 @@
 ## Technically this could become arrow-based
 
 #' submit forecast to EFI
-#' 
+#'
 #' @inheritParams forecast_output_validator
 #' @param metadata path to metadata file
 #' @param ask should we prompt for a go before submission?
 #' @param s3_region subdomain (leave as is for EFI challenge)
 #' @param s3_endpoint root domain (leave as is for EFI challenge)
 #' @export
-submit <- function(forecast_file, 
-                   metadata = NULL, 
-                   ask = interactive(), 
+submit <- function(forecast_file,
+                   metadata = NULL,
+                   ask = interactive(),
                    s3_region = "submit",
-                   s3_endpoint = "ecoforecast.org" 
+                   s3_endpoint = "ecoforecast.org"
 ){
   if(file.exists("~/.aws")){
     warning(paste("Detected existing AWS credentials file in ~/.aws,",
@@ -20,142 +20,86 @@ submit <- function(forecast_file,
   }
   message("validating that file matches required standard")
   go <- forecast_output_validator(forecast_file)
-  
+
   if(!go){
-    
+
     warning(paste0("forecasts was not in a valid format and was not submitted\n",
                    "First, try read reinstalling neon4cast (remotes::install_github('eco4cast\\neon4cast'), restarting R, and trying again\n",
                    "Second, see https://projects.ecoforecast.org/neon4cast-docs/Submission-Instructions.html for more information on the file format"))
     return(NULL)
   }
-  
+
   googlesheets4::gs4_deauth()
   message("Checking if model_id is registered")
-  registered_model_id <- suppressMessages(googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1f177dpaxLzc4UuQ4_SJV9JWIbQPlilVnEztyvZE6aSU/edit?usp=sharing", range = "Sheet1!A:A"))
-  
+  registered_model_id <- suppressMessages(googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1f177dpaxLzc4UuQ4_SJV9JWIbQPlilVnEztyvZE6aSU/edit?usp=sharing", range = "Sheet1!A:V"))
+
+  registered_project_id <- registered_model_id$`What forecasting challenge are you registering for?`
+  registered_model_id <- registered_model_id$model_id
+
+  registered_model_project_id <- paste(registered_project_id, registered_model_id, sep = "-")
+
   df <- read4cast::read_forecast(forecast_file)
   model_id <- df$model_id[1]
-  
+  model_project_id <- paste("neon4cast", registered_model_id, sep = "-")
+
   if(grepl("(example)", model_id)){
     message(paste0("You are submitting a forecast with 'example' in the model_id. As an example forecast, it will be processed but not used in future analyses.\n",
                    "No registration is required to submit an example forecast.\n",
                    "If you want your forecast to be retained, please select a different model_id that does not contain `example` and register you model id at https://forms.gle/kg2Vkpho9BoMXSy57\n"))
   }
-  if(!(model_id %in% registered_model_id$model_id) & !grepl("(example)",model_id)){
-    
-    message("Checking if model_id is already used in submissions")
-    
+
+  if(!(model_project_id %in% registered_model_project_id) & !grepl("(example)",model_id)){
+
+    message("Checking if model_id for neon4cast is already used in submissions")
+
     submitted_model_ids <- readr::read_csv("https://sdsc.osn.xsede.org/bio230014-bucket01/challenges/inventory/model_id/model_id-project_id-inventory.csv", show_col_types = FALSE)
-    
-    if(model_id %in% submitted_model_ids$model_id){
-      
+    submitted_project_model_id <- paste(submitted_model_ids$project_id, submitted_model_ids$model_id, sep = "-")
+
+
+    if(model_project_id %in% submitted_project_model_id){
+
       stop(paste0("Your model_id (",model_id,") has not been registered yet but is already used in other submissions.  Please use and register another model_id\n",
                   "   Register at https://forms.gle/kg2Vkpho9BoMXSy57\n",
                   "If you want to submit without registering, include the word 'example' in your model_id.  As an example forecast, it will be processed but not used in future analyses."))
-      
+
     }else{
-      
+
       stop(paste0("Your model_id (",model_id,") has not been registered\n",
                   "   Register at https://forms.gle/kg2Vkpho9BoMXSy57\n",
                   "If you want to submit without registering, include the word 'example' in your model_id.  As an example forecast, it will be processed but not used in future analyses."))
-      
+
     }
   }
-  
+
   if(!grepl("(example)",model_id)){
-    if(first_submission & model_id %in% registered_model_id$model_id){
+    if(first_submission & model_project_id %in% registered_model_project_id){
       submitted_model_ids <- readr::read_csv("https://sdsc.osn.xsede.org/bio230014-bucket01/challenges/inventory/model_id/model_id-project_id-inventory.csv", show_col_types = FALSE)
-      if(model_id %in% submitted_model_ids$model_id){
+      submitted_project_model_id <- paste(submitted_model_ids$project_id, submitted_model_ids$model_id, sep = "-")
+
+      if(model_project_id %in% submitted_project_model_id){
         stop(paste0("Your model_id (",model_id,") is already used in other submitted forecasts. There are two causes for this error: \n
                     - If you have previously submitted a forecast, set the argument `first_submission = FALSE` to remove this error\n
                     - If you have not previously submitted a forecast, this error message means that the model_id has already been registered and used for submissions.  Please register and use another model_id at [https://forms.gle/kg2Vkpho9BoMXSy57](https://forms.gle/kg2Vkpho9BoMXSy57)"))
       }
     }
   }else{
-    message("Since `example` is in your model_id, you are submitting an example forecast that will be processed but only retained for 30-days")
+    message("Since `example` is in your model_id, you are submitting an example forecast that will be processed but not used in future analyses.")
   }
-  
+
   if(go & ask){
     go <- utils::askYesNo("Forecast file is valid, ready to submit?")
   }
 
   #GENERALIZATION:  Here are specific AWS INFO
-  exists <- aws.s3::put_object(file = forecast_file, 
+  exists <- aws.s3::put_object(file = forecast_file,
                      object = basename(forecast_file),
                      bucket = "submissions",
                      region= s3_region,
                      base_url = s3_endpoint)
-  
+
   if(exists){
     message("Thank you for submitting!")
   }else{
     warning("Forecasts was not sucessfully submitted to server. Try again, then contact the Challenge organizers.")
   }
-  
-  
-  
-  
-  if(!is.null(metadata)){
-    if(tools::file_ext(metadata) == "xml"){
-      EFIstandards::forecast_validator(metadata)
-      aws.s3::put_object(file = metadata, 
-                         object = basename(metadata),
-                         bucket = "neon4cast-submissions",
-                         region= s3_region,
-                         base_url = s3_endpoint)
-    }else{
-      warning(paste("Metadata file is not an .xml file",
-                    "Did you incorrectly submit the model description yml file instead of an xml file"))
-    }
-  }
-}
-
-#' Check that submission was successfully processed
-#' 
-#' @param forecast_file Your forecast csv or nc file
-#' @param s3_region subdomain (leave as is for EFI challenge)
-#' @param s3_endpoint root domain (leave as is for EFI challenge)
-#' @export
-check_submission <- function(forecast_file,
-                             s3_region = "data",
-                             s3_endpoint = "ecoforecast.org"){
-  
-  theme <- stringr::str_split_fixed(basename(forecast_file), "-", n = 2)
-  
-  #All forecats are converted into a common file format when they are processed.  This generates that name.
-  #if (grepl("[.]nc$", forecast_file)) {
-  #  base_name <- paste0(tools::file_path_sans_ext(basename(forecast_file)), 
-  #                      ".csv.gz")
-  #}else if (grepl("[.]csv$", forecast_file)) {
-  #  base_name <- paste0(tools::file_path_sans_ext(basename(forecast_file)), 
-  #                      ".csv.gz")
-  #}else if (grepl("[.]csv\\.gz$", forecast_file)) {
-  #  base_name <- basename(forecast_file)
-  #}else {
-  #  message("File is not a .nc, .cvs, or .csv.gz file")
-  #  base_name <- forecast_file
-  #}
-  
-  base_name <- forecast_file
-  
-  exists <- suppressMessages(aws.s3::object_exists(object = file.path("raw", theme[,1], base_name), 
-                                                   bucket = "neon4cast-forecasts",
-                                                   region= s3_region,
-                                                   base_url = s3_endpoint))
-  
-  
-  if(exists){
-    message("Submission was successfully processed")
-  }else{
-    not_in_standard <- suppressMessages(aws.s3::object_exists(object = file.path("not_in_standard", basename(forecast_file)), 
-                                                              bucket = "neon4cast-forecasts",
-                                                              region= s3_region,
-                                                              base_url = s3_endpoint))
-    if(not_in_standard){
-      message("Submission is not in required format. Try running neon4cast::forecast_output_validator on your file to see what the issue may be")
-    }else{
-        message("Your forecast is still in queue to be processed by the server. Please check again in a few hours")
-    }
-  }
-  invisible(exists)
 }
