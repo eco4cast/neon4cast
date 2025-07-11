f22a422373e4bcfb47769824f9e5020a7ffe06d7
rqthomas <rqthomas@vt.edu>
2025-07-11 10:35:32 -0400

    updating validator to include more checks

--- R/forecast_output_validator.R
+++ R/forecast_output_validator.R
@@ -7,11 +7,15 @@ forecast_output_validator <- function(forecast_file){
   
   config <- yaml::read_yaml("https://raw.githubusercontent.com/eco4cast/neon4cast-ci/main/challenge_configuration.yaml")
   
+  config <- yaml::read_yaml("challenge_configuration.yaml")
+  
+  
   allowed_combinations <- NULL
   for(i in 1:length(config$variable_groups)){
     
     curr_tibble <- data.frame(variable = unlist(config$variable_groups[i][[1]]$variable),
-                              duration = unlist(config$variable_groups[i][[1]]$duration))
+                              duration = unlist(config$variable_groups[i][[1]]$duration),
+                              max_horizon = unlist(config$variable_groups[i][[1]]$max_horizon))
     
     allowed_combinations <- rbind(allowed_combinations, curr_tibble)
   }
@@ -29,6 +33,12 @@ forecast_output_validator <- function(forecast_file){
     
     if(lexists(out, c("model_id"))){
       usethis::ui_done("file has model_id column")
+      if(length(unique(out$model_id)) == 1){
+        usethis::ui_done("only one unique value in the model_id column")
+      }else{
+        usethis::ui_warn("file has more than one unique value in the model_id column.  Only one model_id per submission file is allowed")
+        valid <- FALSE
+      }
     }else{
       usethis::ui_warn("file missing model_id column ")
     }
@@ -43,6 +53,8 @@ forecast_output_validator <- function(forecast_file){
     
     unique_variables <- unique(out$variable)
     
+    # NEED TO GENERALIZE FOR MULTIPLE DURATIONS
+    
     for(i in 1:length(unique_variables)){
       if (unique_variables[i] %in% allowed_combinations$variable) {
         usethis::ui_done(paste0(unique_variables[i], " is a valid variable name"))
@@ -52,6 +64,8 @@ forecast_output_validator <- function(forecast_file){
       }
     }
     
+    ######## PARAMETER FAMILY CHECK #############
+    
     if(lexists(out, "ensemble")){
       usethis::ui_warn("ensemble dimension should be named parameter")
       valid <- FALSE
@@ -59,15 +73,28 @@ forecast_output_validator <- function(forecast_file){
       
       if(lexists(out, "parameter")){
         usethis::ui_done("file has correct family and parameter columns")
+        
+        
+        if(length(unique(out$family)) == 1){
+          usethis::ui_done("only one unique value in the family column")
+          if(unique(out$family) %in% c("ensemble", "sample") & length(unique(out$parameter)) > 500){
+            usethis::ui_warn("file has too many ensemble members.  Needs to be less than 500")
+            valid <- FALSE
+          }
+        }else{
+          usethis::ui_warn("file has more than one unique value in the family column.  Only one family type per model_id is allowed")
+          valid <- FALSE
+        }
       }else{
         usethis::ui_warn("file does not have parameter column ")
         valid <- FALSE
       }
-      
     }else{
       usethis::ui_warn("file does not have ensemble or family and/or parameter column")
       valid <- FALSE
     }
+    
+    ######## SITE_ID COLUMN CHECK #############
     
     #usethis::ui_todo("Checking that file contains siteID column...")
     if(lexists(out, c("site_id"))){
@@ -112,7 +139,7 @@ forecast_output_validator <- function(forecast_file){
     if(lexists(out, c("project_id"))){
       usethis::ui_done("file has project_id column")
     }else{
-      usethis::ui_warn("file missing project_id column (use `neon4cast` as the project_id")
+      usethis::ui_warn("file missing project_id column (use `neon4cast` as the project_id)")
     }
     
     if(lexists(out, c("reference_datetime"))){
@@ -124,10 +151,27 @@ forecast_output_validator <- function(forecast_file){
       valid <- FALSE
     }
     
+    if(lexists(out, c("reference_datetime")) & lexists(out, c("datetime"))){
+      out$horizon <- as.integer(as.POSIXct(out$datetime) - as.POSIXct(out$reference_datetime))/ (60*60*24)
+      
+      for(i in 1:length(unique_variables)){
+        
+        max_horizon <- max(out$horizon[which(out$variable == unique_variables[i])])
+        # NEED TO GENERALIZE FOR MULTIPLE DURATIONS
+        allowed_horizon <- allowed_combinations$max_horizon[which(allowed_combinations$variable == unique_variables[i])][1]
+        
+        if (max_horizon > allowed_horizon){
+          usethis::ui_done(paste0("submitted horizon (as.integer(as.POSIXct(out$datetime) - as.POSIXct(out$reference_datetime))/ (60*60*24)) for variable ", unique_variables[i]," is longer than the allowed ", allowed_horizon, " days"))
+          valid <- FALSE
+        }
+      }
+    }
+
   }else{
     usethis::ui_warn("incorrect file extension (csv or csv.gz are accepted)")
     valid <- FALSE
   }
+  
   if(!valid){
     message("Forecast file is not valid. The following link provides information about the format:\nhttps://projects.ecoforecast.org/neon4cast-ci/instructions.html#forecast-file-format")
   }else{
